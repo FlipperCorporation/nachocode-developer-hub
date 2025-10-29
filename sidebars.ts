2c179ab9da7e31a405aefa4bf97e364a77e3c221
Jiny <jiny@nachocode.io>
2025-10-29 15:20:55 +0900

    [Docs : AppSource ReleaseNote] iOS,AOS 1.8.0 앱소스 릴리즈 노트 작성

--- sidebars.ts
+++ sidebars.ts
@@ -264,6 +264,7 @@ const sidebars: SidebarsConfig = {
           },
           items: [
             'releases/v1/app-source/android/intro',
+            'releases/v1/app-source/android/release-v-1-8-0',
             'releases/v1/app-source/android/release-v-1-7-1',
             'releases/v1/app-source/android/release-v-1-7-0',
             'releases/v1/app-source/android/release-v-1-6-11',
@@ -302,6 +303,7 @@ const sidebars: SidebarsConfig = {
           },
           items: [
             'releases/v1/app-source/ios/intro',
+            'releases/v1/app-source/ios/release-v-1-8-0',
             'releases/v1/app-source/ios/release-v-1-7-1',
             'releases/v1/app-source/ios/release-v-1-7-0',
             'releases/v1/app-source/ios/release-v-1-6-12',
